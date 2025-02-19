14fc1bdd367fa99cee23c4140a59a38589791101
NjugunaKelvin <njugunak349@gmail.com>
2025-02-19 11:17:22 +0300

    add image field to News model and create news template for displaying news articles

--- news/models.py
+++ news/models.py
@@ -5,3 +5,4 @@ class News(models.Model):
     headline = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateField()
+    image = models.ImageField(upload_to='news/images/', null=True, blank=True)
