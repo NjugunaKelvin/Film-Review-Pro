86854d34e6f853b424a79a2b4702a69b91d4b2a8
NjugunaKelvin <njugunak349@gmail.com>
2025-02-19 17:14:30 +0300

    ..

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
     image = models.ImageField(upload_to='movies/images/')
     url = models.URLField(blank=True)
