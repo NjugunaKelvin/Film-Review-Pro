f3953b4afec42942a2227dee42a8b9541aefcbc3
NjugunaKelvin <njugunak349@gmail.com>
2025-02-20 20:14:47 +0300

    add accounts app and integrate URL routing for user signup

--- FILM_REVIEW/urls.py
+++ FILM_REVIEW/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('about/',movieViews.about),
     path('news/',include('news.urls')),
     path('movie/',include('movies.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- movies/models.py
+++ movies/models.py
@@ -5,3 +5,6 @@ class Movie(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='movies/images/')
     url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
