0e0566d8039b4355996dc2b8948539b7e66523f4
NjugunaKelvin <njugunak349@gmail.com>
2025-02-20 20:51:02 +0300

    add error handling and user login to signup process

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
+# for users
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your views here.
 def signupaccount(request):
-    return render(request, 'signupaccount.html', {'form' : UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'signupaccount.html', {'form' : UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signupaccount.html',
+                {'form' : UserCreationForm, 'error' : 'Passwords do not match'})
