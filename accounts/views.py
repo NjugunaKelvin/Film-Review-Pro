95215241d19060492fa934c555cb8dd9ae380472
NjugunaKelvin <njugunak349@gmail.com>
2025-02-21 15:00:25 +0300

    add login and logout functionality with corresponding views and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreateForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # for users
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 
 from django.db import IntegrityError
 
@@ -26,3 +27,20 @@ def signupaccount(request):
         else:
             return render(request, 'signupaccount.html',
                 {'form' : UserCreateForm, 'error' : 'Passwords do not match'})
+        
+        
+def logoutaccount(request):
+    logout(request)
+    return redirect('home') 
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form' : AuthenticationForm})
+    
+    else:
+        user = authenticate(request,username=request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form' : AuthenticationForm(), 'error' : 'Username and Password do not match'})
+        else:
+            login(request, user)
+            return redirect('home')
