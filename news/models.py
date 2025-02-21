125f3eac9a8898f0b7f1399de7d2b1b10d16bce5
NjugunaKelvin <njugunak349@gmail.com>
2025-02-21 19:50:25 +0300

    add URL field to News model; update news template to include links and improve layout

--- news/models.py
+++ news/models.py
@@ -6,6 +6,7 @@ class News(models.Model):
     body = models.TextField()
     date = models.DateField()
     image = models.ImageField(upload_to='news/images/', null=True, blank=True)
+    url = models.URLField(null=True)
 
     # better name in admin site
     def __str__(self):
