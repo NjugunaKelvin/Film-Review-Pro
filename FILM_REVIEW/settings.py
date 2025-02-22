68cbdd62f00985cabeee09314fb143e38215c641
NjugunaKelvin <njugunak349@gmail.com>
2025-02-22 15:48:07 +0300

    Update settings and enhance login template with responsive design

--- FILM_REVIEW/settings.py
+++ FILM_REVIEW/settings.py
@@ -18,7 +18,15 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "film-review-pro.onrender.com",
+    "127.0.0.1"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://film-review-pro.onrender.com"
+]
 
 
 # Application definition
